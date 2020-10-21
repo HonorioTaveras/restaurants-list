6eb77609961903239dc3667bf5119157d489a7ec
Honorio <htaveras90@gmail.com>
2020-10-21 16:08:31 -0600

    complete filtering state functionality

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,4 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+
+import uniq from 'lodash.uniq';
 
 import RESTAURANT_DATA from '../../restaurantData';
 
@@ -8,37 +10,42 @@ import RestaurantItem from '../RestaurantItem/RestaurantItem';
 import './App.scss';
 
 const App = () => {
-  const [restaurantData, setRestaurantData] = useState(RESTAURANT_DATA);
-  const [filter, setFilter] = useState('all');
-
-  // useEffect(() => {
-  //   fetch('http://localhost:3001/').then((res) => res.json());
-  // }, []);
+  const [restaurantData] = useState(RESTAURANT_DATA);
+  // eslint-disable-next-line no-unused-vars
+  const [stateCategories, setStateCetegories] = useState('all');
+  const [states] = useState(
+    uniq(restaurantData.map(({ state }) => state)),
+  );
+  // eslint-disable-next-line prefer-const
+  let [filteredRestaurantData, setFilteredRestaurantData] = useState([]);
 
   const filterStateHandler = (filterState) => {
-    const filteredRestaurantData = [];
+    filteredRestaurantData = [];
     restaurantData.map((restaurant) => {
       if (restaurant.state === filterState) {
         filteredRestaurantData.push(restaurant);
       }
       return filteredRestaurantData;
     });
-    setFilter(filterState);
-    setRestaurantData(filteredRestaurantData);
+    setStateCetegories(filterState);
+    setFilteredRestaurantData(filteredRestaurantData);
   };
 
   return (
     <div>
       <table>
-        <Restaurants
-          restaurantData={restaurantData}
-          filterStateHandler={filterStateHandler}
-        />
-        {restaurantData
-          .sort((a, b) => a.name.localeCompare(b.name))
-          .map((restaurant) => (
-            <RestaurantItem key={restaurant.id} restaurant={restaurant} />
-          ))}
+        <Restaurants states={states} filterStateHandler={filterStateHandler} />
+        {filteredRestaurantData.length
+          ? filteredRestaurantData
+            .sort((a, b) => a.name.localeCompare(b.name))
+            .map((restaurant) => (
+              <RestaurantItem key={restaurant.id} restaurant={restaurant} />
+            ))
+          : restaurantData
+            .sort((a, b) => a.name.localeCompare(b.name))
+            .map((restaurant) => (
+              <RestaurantItem key={restaurant.id} restaurant={restaurant} />
+            ))}
       </table>
     </div>
   );
--- client/src/components/Restaurants/Restaurants.jsx
+++ client/src/components/Restaurants/Restaurants.jsx
@@ -1,34 +1,29 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable react/no-array-index-key */
 import React from 'react';
 
-import uniq from 'lodash.uniq';
-
 import './Restaurants.scss';
 
-const Restaurants = ({ restaurantData, filterStateHandler }) => {
-  const uniqStates = uniq(restaurantData.map(({ state }) => state));
-
-  console.log('states: ', uniqStates);
-  return (
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>City</th>
-        <th>
-          <label>State &nbsp;</label>
-          <select onChange={(e) => filterStateHandler(e.target.value)}>
-            {uniqStates.map((state, idx) => (
-              <option key={idx} value={state}>
-                {state}
-              </option>
-            ))}
-          </select>
-        </th>
-        <th>Telephone</th>
-        <th>Genre &nbsp;</th>
-      </tr>
-    </thead>
-  );
-};
+const Restaurants = ({ states, filterStateHandler }) => (
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>City</th>
+      <th>
+        <label>State &nbsp;</label>
+        <select onChange={(e) => filterStateHandler(e.target.value)}>
+          <option value="all">All States</option>
+          {states.map((state, idx) => (
+            <option key={idx} value={state}>
+              {state}
+            </option>
+          ))}
+        </select>
+      </th>
+      <th>Telephone</th>
+      <th>Genre &nbsp;</th>
+    </tr>
+  </thead>
+);
 
 export default Restaurants;
